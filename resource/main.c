a5fe900f44c468aeee957cb0194d39f7847d922e
1852489 <49758405+1852489@users.noreply.github.com>
2023-05-14 15:50:59 +0800

    change

--- resource/main.c
+++ resource/main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
-int main(){
-    int age=0;
-    scanf_s("%d",&age);
-    printf("hello world:%d",age);
+int main()
+{
+    int age = 0;
+    printf("please input your age:");
+    scanf_s("%d", &age);
+    printf("hello world:%d", age);
     return 0;
 }
